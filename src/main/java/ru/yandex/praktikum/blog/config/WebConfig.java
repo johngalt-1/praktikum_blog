4070a78b2df72187071d785b62cf7f41045b7ae3
Ilya <ilya>
2025-02-22 19:26:27 +0100

    Migrate to spring boot

--- src/main/java/ru/yandex/praktikum/blog/config/WebConfig.java
+++ src/main/java/ru/yandex/praktikum/blog/config/WebConfig.java
@@ -1,17 +1,11 @@
 package ru.yandex.praktikum.blog.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@ComponentScan(basePackages = "ru.yandex.praktikum.blog")
-@PropertySource("classpath:properties/application.properties")
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     private final String imagesDir;
